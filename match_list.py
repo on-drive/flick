b2bbf51bb23e05344d38d7aea5ba1899c1332f06
ooyeraju <rajnishkush2@gmail.com>
2021-05-16 00:50:20 +0530

    fetched match score

--- match_list.py
+++ match_list.py
@@ -2,38 +2,52 @@ import requests
 import json
 
 
-headers = {
-    'authority': 'hs-consumer-api.espncricinfo.com',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36',
-    'accept': '*/*',
-    'origin': 'https://www.espncricinfo.com',
-    'sec-fetch-site': 'same-site',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-dest': 'empty',
-    'referer': 'https://www.espncricinfo.com/',
-    'accept-language': 'en-US,en;q=0.9',
-}
+def get_match_details(series_id, match_id):
+    match_link = "https://hs-consumer-api.espncricinfo.com/v1/pages/match/details?lang=en&seriesId={series_id}&matchId={match_id}&latest=true".format(
+        series_id=series_id, match_id=match_id
+    )
+    detail_res = requests.get(match_link).text
 
-params = (
-    ('latest', 'true'),
-)
+    match_detail_data = json.loads(detail_res)["match"]
 
-response = requests.get('https://hs-consumer-api.espncricinfo.com/v1/pages/matches/current', headers=headers, params=params)
+    # detail_list = [
+    #     {
+    #         "status_text": detail["statusText"],
+    #     }
+    #     for detail in match_detail_data
+    # ]
 
-#NB. Original query string below. It seems impossible to parse and
-#reproduce query strings 100% accurately so the one below is given
-#in case the reproduced version is not "correct".
-response = requests.get('https://hs-consumer-api.espncricinfo.com/v1/pages/matches/current?latest=true', headers=headers).text
+    status_text = match_detail_data["statusText"]
 
-match_data = json.loads(response)["matches"]
-
-match_list = [{
-                "match_name": match["slug"],
-                "match_id": match["objectId"],
-                "series_id" : match["series"]["objectId"],
-            } for match in match_data]
-
-print(match_list)
+    print(status_text)
 
 
+def get_match_list():
+    response = requests.get(
+        "https://hs-consumer-api.espncricinfo.com/v1/pages/matches/current?latest=true"
+    ).text
 
+    match_data = json.loads(response)["matches"]
+
+    match_list = [
+        {
+            "match_name": match["slug"],
+            "match_id": match["objectId"],
+            "series_id": match["series"]["objectId"],
+        }
+        for match in match_data
+    ]
+    print("Choose any one of the following: \n")
+    for i in range(0, len(match_list)):
+        print(str(i) + " " + match_list[i]["match_name"])
+
+    user_choice = int(input("\nEnter match number to get recent updates: "))
+
+    # print(str(match_list[user_choice]["match_id"]))
+
+    get_match_details(
+        match_list[user_choice]["series_id"], match_list[user_choice]["match_id"]
+    )
+
+
+get_match_list()
