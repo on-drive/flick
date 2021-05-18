af97e07f279c5201029b49fb06566a2a93869257
Abhinav kumar <abhinav72610@gmail.com>
2021-05-19 01:16:14 +0530

    error handled using try except

--- cric_info.py
+++ cric_info.py
@@ -18,23 +18,27 @@ def get_match_details(series_id, match_id):
 
 
 def get_match_dict_list():
-    response = requests.get(
-        "https://hs-consumer-api.espncricinfo.com/v1/pages/matches/current?latest=true"
-    ).text
+    match_dict_list = []
+    try:
+        response = requests.get(
+            "https://hs-consumer-api.espncricinfo.com/v1/pages/matches/current?latest=true"
+        ).text
 
-    match_data = json.loads(response)["matches"]
+        match_data = json.loads(response)["matches"]
 
-    match_dict_list = [
-        {
-            "match_name": match["slug"],
-            "match_id": match["objectId"],
-            "series_id": match["series"]["objectId"],
-            "team-1": match["teams"][0]["team"]["longName"],
-            "team-2": match["teams"][1]["team"]["longName"],
-        }
-        for match in match_data
-        if match["status"] == "Live"
-    ]
+        match_dict_list = [
+            {
+                "match_name": match["slug"],
+                "match_id": match["objectId"],
+                "series_id": match["series"]["objectId"],
+                "team-1": match["teams"][0]["team"]["longName"],
+                "team-2": match["teams"][1]["team"]["longName"],
+            }
+            for match in match_data
+            if match["status"] == "Live"
+        ]
+    except Exception:
+        pass    
     match_list = ""
     # print("Choose any one of the following: \n")
     for i in range(0, len(match_dict_list)):
