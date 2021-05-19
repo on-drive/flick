dfe1eb6e4a889088c2a9d2e354b280430c2d0fa6
Abhinav kumar <abhinav72610@gmail.com>
2021-05-19 19:23:01 +0530

    setonce handler added

--- constants.py
+++ constants.py
@@ -4,6 +4,7 @@ help_text = """Here's a list of all commands that you may use:
 /matches - Shows the list of current matches along with index which can be used to trigger other commands.
 /set - Takes in 2 argument first is the timeinterval (in sec) and second is the match index. 
 (For eg - '/set 5 0' -> This will give updates about the 0th match in every 5 seconds)
+/setonce - This command can be used when you only want the data once.
 /unset - It unsets the timer and you'll not recive any futher updates. 
 
 Regards
@@ -13,7 +14,7 @@ default_text = "Sorry! We don't entertain this command. Type /help to know more.
 negative_time = "Sorry we can not go back to future!"
 negative_match = "Enter a valid match number!"
 timer_success = "Timer successfully set!"
-except_reply = 'Usage: /set <seconds> <match index>'
+except_reply = "Usage: /set <seconds> <match index>"
 live_matches = "Here's a list of current live matches:\n"
 timer_cancel = "Timer successfully cancelled!"
 no_timer = "You have no active timer."
